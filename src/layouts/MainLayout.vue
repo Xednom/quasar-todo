98c35e185dbfc8d9fd571e5e3309345635f2468f
Xednom <monde.lacanlalay@gmail.com>
2023-11-02 19:46:20 +0800

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,95 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todo</div>
+        <div class="text-subtitle1">{{ todaysDate }}</div>
+      </div>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      :width="200"
+      :breakpoint="400"
+    >
+      <q-scroll-area
+        style="
+          height: calc(100% - 150px);
+          margin-top: 150px;
+          border-right: 1px solid #ddd;
+        "
+      >
+        <q-list padding>
+          <q-item to="/" exact clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
+
+            <q-item-section> Todo </q-item-section>
+          </q-item>
+
+          <q-item to="/help" exact clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+
+            <q-item-section> Help </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <q-img
+        class="absolute-top"
+        src="https://cdn.quasar.dev/img/material.png"
+        style="height: 150px"
+      >
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="https://www.bvgels.dev/profile.bfd5dbaf.jpg" />
+          </q-avatar>
+          <div class="text-weight-bold">Richmond B. Lacanlalay</div>
+          <div>@_mundii</div>
+        </div>
+      </q-img>
+    </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from "quasar";
+
+export default {
+  name: "MainLayout",
+  components: {},
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+  computed: {
+    todaysDate() {
+      const timeStamp = Date.now();
+      const formattedString = date.formatDate(timeStamp, "dddd D MMMM");
+
+      return formattedString;
+    },
+  },
+};
+</script>
---
+++ src/pages/Help.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Quisquam molestiae officia explicabo totam in. Eveniet voluptatem pariatur
+      quod. Fugiat temporibus voluptas a aut eligendi quae. Recusandae quo velit
+      necessitatibus aut perspiciatis. Ut ipsa voluptatibus id sed et blanditiis
+      et sit. Sit recusandae fuga aliquid eveniet reprehenderit dolorum sint.
+    </p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "Help",
+};
+</script>
